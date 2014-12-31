3e246a868b142bea1fcd2353b1b93c23e7c09094
boruxu <boruxu@qq.com>
2014-12-31 18:53:26 +0800

    修改接口，修改出错的js文件

--- src/main/java/cn/edu/buaa/g305/qpm/correlation/server/imp/CorrelationImp.java
+++ src/main/java/cn/edu/buaa/g305/qpm/correlation/server/imp/CorrelationImp.java
@@ -9,11 +9,12 @@ import cn.edu.buaa.g305.qpm.correlation.server.Correlation;
 @Component
 public class CorrelationImp implements Correlation{
 
-	@Override
-	public Map<String, Double> computeCorrelationAndPValue(double[] xArray,double[] yArray) {
-		
-		
+	public Map<String, Double> computeCorrelationAndPValue(double[] xArray,
+			double[] yArray) {
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+
 	
 }
