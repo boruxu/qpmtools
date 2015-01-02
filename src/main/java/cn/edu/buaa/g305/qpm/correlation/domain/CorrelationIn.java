b1cd39af4ee2846ebb93e97e06aeceb3c3493c85
boruxu <boruxu@qq.com>
2015-01-02 17:07:29 +0800

    修改输入和输出数据为domain域模型

---
+++ src/main/java/cn/edu/buaa/g305/qpm/correlation/domain/CorrelationIn.java
@@ -0,0 +1,15 @@
+package cn.edu.buaa.g305.qpm.correlation.domain;
+
+public class CorrelationIn {
+	
+	private CorrelationInXYArray[] correlationIn;
+
+	public CorrelationInXYArray[] getCorrelationIn() {
+		return correlationIn;
+	}
+
+	public void setCorrelationIn(CorrelationInXYArray[] correlationIn) {
+		this.correlationIn = correlationIn;
+	}
+
+}
---
+++ src/main/java/cn/edu/buaa/g305/qpm/correlation/domain/CorrelationInXYArray.java
@@ -0,0 +1,20 @@
+package cn.edu.buaa.g305.qpm.correlation.domain;
+
+public class CorrelationInXYArray {
+	
+	private double[] x;
+	private double[] y;
+	public double[] getX() {
+		return x;
+	}
+	public void setX(double[] x) {
+		this.x = x;
+	}
+	public double[] getY() {
+		return y;
+	}
+	public void setY(double[] y) {
+		this.y = y;
+	}
+
+}
