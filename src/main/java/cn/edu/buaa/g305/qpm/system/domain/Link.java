d12c00caf4cdf90aff3942f9226a1de46de7b7cb
boruxu <boruxu@qq.com>
2015-02-01 23:00:27 +0800

    之前版本漏传文件

---
+++ src/main/java/cn/edu/buaa/g305/qpm/system/domain/Link.java
@@ -0,0 +1,35 @@
+package cn.edu.buaa.g305.qpm.system.domain;
+
+public class Link {
+	//超媒体链接含义
+	private String rel;
+	//超媒体链接
+	private String href;
+	
+	public Link(){
+		
+	}
+	
+    public Link(String rel,String href){
+    	
+    	this.rel=rel;
+    	this.href=href;
+		
+	}
+
+	public String getRel() {
+		return rel;
+	}
+	public void setRel(String rel) {
+		this.rel = rel;
+	}
+	public String getHref() {
+		return href;
+	}
+	public void setHref(String href) {
+		this.href = href;
+	}
+	
+	
+
+}
